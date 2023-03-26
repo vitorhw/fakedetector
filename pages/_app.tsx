8bd9b2ec67b6dbc916a90e9c94c7bc2c329e7fda
vitorhw <vitor.hw@outlook.com>
2023-03-26 00:16:39 -0300

    GTag update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,13 +20,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
-        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
-  function gtag(){window.dataLayer.push(arguments)}
+  function gtag(){window.dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});`}
